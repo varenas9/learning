717362dd7d40762e466401205fccbbd5cb642a00
Vanessa <Vane@Vanessas-MacBook-Pro.local>
2018-09-21 19:57:06 -0500

    conditionals added with switch and case

--- Intro.js
+++ Intro.js
@@ -1,4 +1,19 @@
 'use strict';
 const chalk = require('chalk');
-console.log(chalk.underline.bgGreen.yellow.bold('Hello World!'));
-let chocolate = 4;  
+console.log(chalk.bgBlue.red.bold('Hello World!'));
+
+let sport = 'Horseback riding';
+switch(sport){
+    case 'Taekwondo':
+console.log(chalk.bgYellow.blue('This is a great martial art!'));
+break;
+case 'Soccer':
+console.log(chalk.bgRed.yellow('This sport is great cardio!'));
+break;
+case 'Basketball':
+console.log(chalk.bgYellow.red('Jump for those hoops!'));
+break;
+default:
+console.log('Go play some sport!');
+break;
+}
