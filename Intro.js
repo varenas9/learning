47e760d8c51e3af4cd835b81dee85d00e70b983e
Vanessa <Vane@Vanessas-MacBook-Pro.local>
2018-09-22 11:19:37 -0500

    changed let to const

--- Intro.js
+++ Intro.js
@@ -2,18 +2,18 @@
 const chalk = require('chalk');
 console.log(chalk.bgBlue.red.bold('Hello World!'));
 
-let sport = 'Horseback riding';
-switch(sport){
-    case 'Taekwondo':
-console.log(chalk.bgYellow.blue('This is a great martial art!'));
-break;
-case 'Soccer':
-console.log(chalk.bgRed.yellow('This sport is great cardio!'));
-break;
-case 'Basketball':
-console.log(chalk.bgYellow.red('Jump for those hoops!'));
-break;
-default:
-console.log('Go play some sport!');
-break;
+const sport = 'Horseback riding';
+switch (sport){
+  case 'Taekwondo':
+    console.log(chalk.bgYellow.blue('This is a great martial art!'));
+    break;
+  case 'Soccer':
+    console.log(chalk.bgRed.yellow('This sport is great cardio!'));
+    break;
+  case 'Basketball':
+    console.log(chalk.bgYellow.red('Jump for those hoops!'));
+    break;
+  default:
+    console.log('Go play some sport!');
+    break;
 }
