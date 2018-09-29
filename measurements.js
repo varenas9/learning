1653871e27298cb4ebc2623bf5aaeff142d59535
Vanessa Arenas <varenas9@gmail.com>
2018-09-28 19:31:41 -0500

    temperature and teaspoons added

---
+++ measurements.js
@@ -0,0 +1,10 @@
+'use strict';
+const temperature = require('./lib/farenheitToCelsius');
+const ounces = require('./lib/teaspoontoounces');
+
+
+
+
+const teaspoon = 1;
+
+const ounce = teaspoon * 2;
