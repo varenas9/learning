987e9d10afb59002f7d35662411d40fd7060dcd7
Vanessa Arenas <varenas9@gmail.com>
2018-09-30 20:49:46 -0500

    measurements added

--- measurements.js
+++ measurements.js
@@ -1,10 +1,19 @@
 'use strict';
 const temperature = require('./lib/farenheitToCelsius');
 const ounces = require('./lib/teaspoontoounces');
+const cups = require('./lib/cupsToMl');
+const color = require('./lib/color');
 
 
 
+const milk = 2;
+const vanilla = 1 / 2;
+const oven = 375;
 
-const teaspoon = 1;
+const milkInMl = cups(milk) * 2;
+const vanillaInOunces = ounces(vanilla) / 0.5;
+const tempInCelsius = temperature(oven);
 
-const ounce = teaspoon * 2;
+console.log(color('Milk needed in MLs:' + milkInMl, 'yellow'));
+console.log(color('Vanilla needed in ounces:' + vanillaInOunces, 'blue'));
+console.log(color('Preheat your oven to celsius:' + tempInCelsius, 'red'));
