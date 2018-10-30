7d7d1a61cbeebcf5e9764733081a07d9cfb7333d
Vanessa Arenas <varenas9@gmail.com>
2018-10-30 08:21:14 -0500

    callbacks

--- test/lib/cuentas.js
+++ test/lib/cuentas.js
@@ -7,12 +7,14 @@ const cuentas = require('../../lib/cuentas');
 describe('cuentas', () => {
   describe('caulcular total', () => {
     it('should calculate total when there is no tip on itbms', () => {
-      const results = cuentas(5, .10, false);
-      expect(results).to.equal(5.85);
+      cuentas(5, .10, false, (results) => {
+        expect(results).to.equal(5.85);
+      });
     });
-    it('should calculate the total when tip is on itbms', () => {
-      const results = cuentas(5, 0.10, true);
-      expect(results).to.equal(5.89);
+    it('should calculate the total when tip is on itbms', (done) => {
+      cuentas(5, 0.10, true, (results) => {
+        expect(results).to.equal(5.89);
+      });
     });
   });
 });
