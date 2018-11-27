39dc9fd55a964686d557ea96d470a263efe2c10c
Vanessa Arenas <varenas9@gmail.com>
2018-11-27 09:00:44 -0500

    js conf problem

--- index.js
+++ index.js
@@ -1,43 +1,92 @@
 'use strict';
-// const color = require('./lib/color');
-// const names = require('./config/personas');
-// const convert = require('./lib/convert');
-// //const juice = color(names[0], 'blue');
-// console.log(color(names[0], 'blue'));
-// console.log(color(names[1], 'green'));
-// console.log(color(convert(7, 'teaspoons', 'Ounces'), 'red'));
-// const Car = require('./lib/car');
-const Human = require('./lib/human');
-// const Dog = require('./lib/dog');
-// const blueCar = new Car('blue');
-// const Fercho = new Human('Fercho');
-// const Mariana = new Human('Mariana');
-// const Alfred = new Dog('Alfred');
-// // const Waffles = new Dog('Waffles');
-
-// const car = Car.buildBlue();
-// console.log(car);
-
-// const car1 = Car.buildRedandWhite();
-// console.log(car1);
-// blueCar.boardCar(Fercho);
-// blueCar.boardCar(Alfred);
-
-// console.log(blueCar);
-
-// console.log(new Car('yellow'));
-// console.log(new Car('green'));
-// console.log(new Car('Red and White'));
+const axios = require('axios');
+const compatibility = {
+  'O-': ['AB+', 'AB-', 'A+', 'A-', 'B+', 'B-', 'O+', 'O-'],
+  'O+': ['AB+', 'A+', 'B+', 'O+'],
+  'B-': ['AB+', 'AB-', 'B+', 'B-'],
+  'B+': ['AB+', 'B+'],
+  'A-': ['AB+', 'AB-', 'A+', 'A-'],
+  'A+': ['AB+', 'A+'],
+  'AB-': ['AB+', 'AB-'],
+  'AB+': ['AB+']
+};
+// const people = {
+//   'Edwin': 'O-',
+//   'Michelle': 'AB+',
+//   'Owen': 'AB+',
+//   'Jorge': 'B-',
+//   'Josh': 'A+',
+//   'Mike': 'O+',
+//   'Lauren': 'O+'
+// };
 
 
-// console.log(blueCar);
-// blueCar.startCar();
-// console.log(blueCar);
-// blueCar.turnOffCar();
-// console.log(blueCar);
+// const donors = {};
 
-const human = Human.createAdult('Mario');
-human.drinksCoffee();
-console.log(human);
-const baby = Human.createBaby('Emiliano');
-console.log(baby);
+// for (const idx in people) {
+//   const bloodType = people[idx];
+//   const compatibleBloodType = compatibility[bloodType];
+
+//   const canReceive = [];
+//   for (const receivers in people) {
+//     const receiverBloodType = people[receivers];
+//     const positiveMatch = compatibleBloodType.includes(receiverBloodType);
+//     if (positiveMatch && receivers !== idx) {
+//       canReceive.push(receivers);
+//     }
+//   }
+//   donors[idx] = canReceive;
+// }
+
+// console.log(donors);
+function horse(response){
+  const authorization = response.headers.authorization;
+  const people = response.data;
+  const donors = calculateDonors(people);
+
+
+  return axios({
+    url: 'https://jsconf.admios.com/blood-types',
+    method: 'post',
+    data: donors,
+    headers: {'Authorization': authorization}
+  });
+}
+axios.get('https://jsconf.admios.com/blood-types')
+  .then((response) => {
+    return horse(response);
+  })
+  .then((response) => {
+    return horse(response);
+  })
+  .then((response) => {
+    return horse(response);
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    if (err.response.status === 404) {
+      console.log('Not Found');
+    }
+  });
+
+function calculateDonors(people) {
+  const donors = {};
+
+  for (const idx in people) {
+    const bloodType = people[idx];
+    const compatibleBloodType = compatibility[bloodType];
+
+    const canReceive = [];
+    for (const receivers in people) {
+      const receiverBloodType = people[receivers];
+      const positiveMatch = compatibleBloodType.includes(receiverBloodType);
+      if (positiveMatch && receivers !== idx) {
+        canReceive.push(receivers);
+      }
+    }
+    donors[idx] = canReceive;
+  }
+  return donors;
+}
